9137f77d96fa40ec6c4fca136f8a9d10c01450d8
dgarc1758 <Garciadarwin305@gmail.com>
2022-09-16 13:35:44 -0500

    Initial Commit

--- src/components/MainCardsSingle.vue
+++ src/components/MainCardsSingle.vue
@@ -1,10 +1,32 @@
 <script setup>
-    import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker'
+const firstName = faker.name.firstName()
+const lastName = faker.name.lastName()
+const fullName = `${firstName} ${lastName}`
+
+const selectCard = () => {
+    console.log(`${fullName} selected`)
+}
 </script>
 
 
 <template>
-    <img :src="faker.internet.avatar" alt="" srcset="" />
-        <span class=" ">{{ faker.name.fullName()}}</span>
-    
+    <div class="card" @click="selectCard">
+     <div class="card-image">
+        <img :src="faker.internet.avatar" alt="" srcset="" />
+     </div>
+     <div class="card-details">
+            <p class="card-details-name">{{ fullName}}</p>
+            <p class="card-details-job">{{ faker.name.jobTitle()}},{{faker.name.jobArea()}}</p>
+            <p class="card-details-email">{{ faker.internet.exampleEmail(firstName, lastName)}}</p>
+            <p class="card-details-quote">{{faker.lorem.paragraph()}}</p>
+      </div>  
+    </div>
 </template>
+
+<style scoped lang="postcss">
+    .card{
+        @apply cursor-pointer overflow-hidden rounded-md bg-slate-100 p-8 shadow-md transition-transform duration-300 hover:scale-110 hover:shadow-2xl hover:shadow-slate-900;
+    }
+
+</style>
