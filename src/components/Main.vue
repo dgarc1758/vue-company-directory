6f0a9c3a55900248e8f5530745068252fff3c37f
dgarc1758 <Garciadarwin305@gmail.com>
2022-09-16 22:13:42 -0500

    created pagination

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,17 @@
 <script setup>
-import Mainsearch from './Mainsearch.vue';
- import MainCards from './MainCards.vue';
+  import Mainsearch from './Mainsearch.vue'
+  import MainCards from './MainCards.vue'
+  import MainPagination from './MainPagination.vue'
+import MainPagination from './MainPagination.vue';
 </script>
 <template>
-    <div class="wrapper">
-    <Mainserch />
+  <div class="wrapper">
+    <Mainsearch />
     <MainCards />
-    </div>
+    <MainPagination />
+  </div>
 </template>
 
 <style scoped lang="postcss">
-    @apply container mx-auto flex flex-col gap-4 py-4;
-
+  @apply container mx-auto flex flex-col gap-4 py-4;
 </style>
